4aa9ca982dd26d1dcc5ab8c65d63b953a596bbf5
a58071 <a58071@alunos.ipb.pt>
2024-07-16 10:25:39 +0100

    exercicio 1 JS30

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="keys">
+        <div data-key="65" className="key key65">
+          <kbd>A</kbd>
+          <span className="sound">atumalaca</span>
+        </div>
+        <div data-key="83" className="key key83">
+          <kbd>S</kbd>
+          <span className="sound">cavalo</span>
+        </div>
+        <div data-key="68" className="key key68">
+          <kbd>D</kbd>
+          <span className="sound">ele gosta</span>
+        </div>
+        <div data-key="70" className="key key70">
+          <kbd>F</kbd>
+          <span className="sound">coringa</span>
+        </div>
+        <div data-key="71" className="key key71">
+          <kbd>G</kbd>
+          <span className="sound">por que será</span>
+        </div>
+        <div data-key="72" className="key key72">
+          <kbd>H</kbd>
+          <span className="sound">Faz o L</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <audio data-key="65" src="sounds/atumalaca.wav"></audio>
+      <audio data-key="83" src="sounds/cavalo.mp3"></audio>
+      <audio data-key="68" src="sounds/ele-gosta.mp3"></audio>
+      <audio data-key="70" src="sounds/coringa.mp3"></audio>
+      <audio data-key="71" src="sounds/porquesera.mp3"></audio>
+      <audio data-key="72" src="sounds/fazol.mp3"></audio>
+      <script>
+        {
+          window.addEventListener("keydown", function (e) {
+          const audio = document.querySelector(
+            `audio[data-key="${e.keyCode}"]`
+          );
+          const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+          console.log(audio);
+          if (!audio) return;
+          audio.currentTime = 0;
+          audio.play();
+
+          key.classList.add("playing");
+          function removeTransition(e){
+            if (e.propertyName !== 'transform') return;
+            this.classList.remove('playing')
+          }
+          const keys = document.querySelectorAll('.key');
+          keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+        })}
+      </script>
+      
+    </>
+  );
+}
